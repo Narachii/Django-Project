174217c54beb3ec320c88c63162038f4f12d571f
Narachii <naoya706oxford@gmail.com>
2017-09-03 16:58:46 +0900

    Created help.html for ProTwo

--- ProTwo/appTwo/views.py
+++ ProTwo/appTwo/views.py
@@ -3,5 +3,9 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-def index(renduest):
-	return HttpResponse("<em> My Second Project</em>")
+def index(request):
+	return HttpResponse("<em> My Second Project! </em>")
+
+def help(request):
+	helpdict = {'help_insert': 'HELP PAGE'}
+	return render(request, 'help.html',context=helpdict)
